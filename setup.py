5359937cca60077131636832e2225e842409deb2
Kyle Barron <barronk@mit.edu>
2019-11-27 18:49:24 -0700

    Declare development status as beta

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author_email='kylebarron2@gmail.com',
     python_requires='>=3.5',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
